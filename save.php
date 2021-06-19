1f0fcd5fd44b93b60ca42bad7c846c4f1762022c
Isaque Menezes da Silva <37957188+isaquemenezes@users.noreply.github.com>
2021-06-19 20:25:19 -0300

    update 6

--- save.php
+++ save.php
@@ -2,7 +2,7 @@
 
     $data = $_POST['img'];
 
-    $data = str_replace('data:image/png:base64,', '', $data);
+    $data = str_replace('data:image/png;base64,', '', $data);
     $data = str_replace(' ', '+', $data);
 
     $img = base64_decode($data);
